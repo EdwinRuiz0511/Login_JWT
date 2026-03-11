775ac972f9f34c30a4b3af1451db539e15dcec51
Edwin Ruiz <eruiz1545@gmail.com>
2026-03-11 15:53:57 -0500

    Agregamos logs a CustomUserDetailsService

--- src/main/java/com/loginJWT/backend/security/serviceSecurity/CustomUserDetailsService.java
+++ src/main/java/com/loginJWT/backend/security/serviceSecurity/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.loginJWT.backend.security.serviceSecurity;
 
 import com.loginJWT.backend.entity.UsuarioEntity;
 import com.loginJWT.backend.repository.IUsuarioRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j                                                                                                                  // Habilita el uso de logs (debug, info, warn, etc.) mediante Lombok
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -19,9 +21,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        UsuarioEntity usuarioEnt = usuarioRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: --> " + username));
+        log.debug("Intentando cargar usuario para la autenticacion: {}",username);
 
+        // Busca el usuario en la base de datos por username
+        UsuarioEntity usuarioEnt = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> { log.warn("Usuario no encontrado: --> {}", username);
+                        return new UsernameNotFoundException("Usuario no encontrado: --> " + username);
+                });
+
+        // Log informativo que confirma que el usuario fue encontrado
+        log.info("Usuario encontrado: {}", usuarioEnt.getUsername());
+
+        // Construye el objeto UserDetails que Spring Security usará para autenticar al usuario
         return User.builder()
                 .username(usuarioEnt.getUsername())
                 .password(usuarioEnt.getPassword())
